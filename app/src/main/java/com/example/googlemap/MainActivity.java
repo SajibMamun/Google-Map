18a84f993859b243e45416eb44ace1bcc9fa712a
SajibMamun <sajib15-3435@diu.edu.bd>
2022-06-15 10:08:33 +0600

    current location added

--- app/src/main/java/com/example/googlemap/MainActivity.java
+++ app/src/main/java/com/example/googlemap/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.googlemap;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,15 +19,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity  implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        checkpermissions();
 
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -28,15 +39,68 @@ public class MainActivity extends AppCompatActivity  implements OnMapReadyCallba
         mapFragment.getMapAsync(this);
     }
 
+    private void checkpermissions() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                },0);
+                return;
+            }
+        }
+    }
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
 
-        LatLng BD=new LatLng(23.7807777,90.3492858);
+        LatLng BD = new LatLng(23.7807777, 90.3492858);
 
         //googleMap.moveCamera(CameraUpdateFactory.newLatLng(BD));
 
-        googleMap.addMarker(new MarkerOptions().position(BD).title("Dhaka").snippet("The capital of Bangladesh"));
-     googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BD,7));
+      //  googleMap.addMarker(new MarkerOptions().position(BD).title("Dhaka").snippet("The capital of Bangladesh"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BD, 7));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            checkpermissions();
+        }
+        googleMap.setMyLocationEnabled(true);
+
+
+        //this is for custom button
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+
+
+    }
+
+    private void currentlocation() {
+
+        FusedLocationProviderClient fusedLocationProviderClient = new FusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            checkpermissions();
+        }
+        Task location = fusedLocationProviderClient.getLastLocation();
+        location.addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                Location currentllocation = (Location) task.getResult();
+
+                map.addMarker(new MarkerOptions().position(new LatLng(currentllocation.getLatitude(),currentllocation.getLongitude())));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentllocation.getLatitude(),currentllocation.getLongitude()),7));
+
+
+            }
+        });
+    }
+
+    public void myLocation(View view) {
+
+        currentlocation();
 
     }
 }
