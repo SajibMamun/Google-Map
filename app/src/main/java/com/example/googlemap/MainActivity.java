9d0b1922537e432380372788a4c1f33d50ee81e9
SajibMamun <sajib15-3435@diu.edu.bd>
2022-06-15 22:40:03 +0600

    Search Location

--- app/src/main/java/com/example/googlemap/MainActivity.java
+++ app/src/main/java/com/example/googlemap/MainActivity.java
@@ -6,10 +6,18 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdate;
@@ -22,14 +30,24 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap map;
+    ImageView searchimagebtn;
+    EditText searchet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initiazlize();
+
+
+
 
         checkpermissions();
 
@@ -38,6 +56,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
+    private void initiazlize() {
+        searchet=findViewById(R.id.SearchETid);
+        searchimagebtn=findViewById(R.id.SearchImageiconid);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void checkpermissions() {
 
@@ -52,16 +87,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+
+
+
+
+
+
+
+
+
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
+
 
         LatLng BD = new LatLng(23.7807777, 90.3492858);
 
         //googleMap.moveCamera(CameraUpdateFactory.newLatLng(BD));
 
       //  googleMap.addMarker(new MarkerOptions().position(BD).title("Dhaka").snippet("The capital of Bangladesh"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BD, 7));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BD, 15));
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
@@ -72,6 +118,62 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         //this is for custom button
         googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        SearchLocation();
+
+
+
+    }
+
+    private void SearchLocation() {
+
+        searchet.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+
+                String search=searchet.getText().toString().trim();
+                if(i== EditorInfo.IME_ACTION_SEARCH
+                ||i==EditorInfo.IME_ACTION_DONE
+                ||keyEvent.getAction()==KeyEvent.ACTION_DOWN
+                ||keyEvent.getAction()==KeyEvent.KEYCODE_ENTER)
+                {
+
+                    Geocoder geocoder=new Geocoder(MainActivity.this);
+
+
+                    List<Address> addressList=new ArrayList<>();
+
+                    try {
+                        addressList=geocoder.getFromLocationName(search,1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(addressList.size()>0)
+                    {
+                        Address address=addressList.get(0);
+
+
+                        LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
+                        map.addMarker(new MarkerOptions().position(latLng).title(address.getAddressLine(0)).snippet(" Courntry Name: " +address.getCountryName()+
+
+                                "Locality: "+address.getLocality()+"Zip Code: "+address.getPostalCode()));
+
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                    }
+
+
+                }
+
+
+
+
+
+
+
+                return false;
+            }
+        });
 
 
 
